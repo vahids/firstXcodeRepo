9b24bebe14e8b6258a1b13a355c86cc83800c5b2
Vahid Sayad <vahidgiga@gmail.com>
2016-05-13 11:23:11 +0430

    Initial Commit

---
+++ FirstGitXcode/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FirstGitXcode
+//
+//  Created by Vahid Sayad on 5/13/16.
+//  Copyright © 2016 Vahid Sayad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
